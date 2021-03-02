d4eaa55941e2d60d2c09c955636e9854b325a1a3
Laurent Yvonnet <yvonnet_l@yahoo.fr>
2021-03-02 10:50:07 +0100

    first commit de mise en place du projet

---
+++ src/main/java/com/safetynet/safetyAlerts/SafetyAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.safetyAlerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyAlertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetynet/safetyAlerts/SafetyAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.safetyAlerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
